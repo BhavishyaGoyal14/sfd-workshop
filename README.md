1e02ce55aec3acd2bd710648d9f40071aa1e9b70
unknown <abhaswithdelrio@gmail.com>
2014-09-28 00:33:52 -0700

    add two functions

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ sfd-workshop
 ============
 
 This repo was used in sfd-smvdu Github Workshop.
+
+multiply function multiplies two numbers
+swap function swaps two numbers
--- function.py
+++ function.py
@@ -1,4 +1,11 @@
-def add(a, b):
-	print a + b
+def multiply(a, b):
+	print a*b
 
-add(2, 3)
+multiply(2,3)
+
+def swap(a,b):	
+	print a, b
+	b, a = a,b
+	print a, b
+
+swap(2,3)
